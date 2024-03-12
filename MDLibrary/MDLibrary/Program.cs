353b32d37ff1009a297aa3a7bc8958c137cfe524
Pater-Forg <gbogdanovg@gmail.com>
2024-03-12 19:08:43 +0300

    add mvc with home controller and view

--- MDLibrary/MDLibrary/Program.cs
+++ MDLibrary/MDLibrary/Program.cs
@@ -5,15 +5,59 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("MDLibraryIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'MDLibraryIdentityDbContextConnection' not found.");
 
-builder.Services.AddDbContext<MDLibraryIdentityDbContext>(options => options.UseSqlServer(connectionString));
+#region Connection
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MDLibraryIdentityDbContext>();
+var identityConnectionString = builder.Configuration.GetConnectionString(
+    "MDLibraryIdentityDbContextConnection")
+        ?? throw new InvalidOperationException(
+            "Connection string 'MDLibraryIdentityDbContextConnection' not found.");
+
+#endregion
+
+#region Services
+
+builder.Services.AddDbContext<MDLibraryIdentityDbContext>(options =>
+    options.UseSqlServer(identityConnectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MDLibraryIdentityDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+#endregion
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region EnvironmentConfiguration
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+#endregion
+
+#region Uses
+
+app.UseRouting();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+#endregion
+
+#region Routes
+
+app.MapRazorPages();
+//app.MapAreaControllerRoute(name: "admin", areaName: "Admin", pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "areas", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+#endregion
 
 app.Run();
