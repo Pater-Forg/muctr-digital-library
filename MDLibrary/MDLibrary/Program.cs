29d62fea350a44ba148f89eea1572a2352832a22
Pater-Forg <gbogdanovg@gmail.com>
2024-03-12 18:26:29 +0300

    add microsoft identity

--- MDLibrary/MDLibrary/Program.cs
+++ MDLibrary/MDLibrary/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using MDLibrary.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MDLibraryIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'MDLibraryIdentityDbContextConnection' not found.");
+
+builder.Services.AddDbContext<MDLibraryIdentityDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MDLibraryIdentityDbContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
